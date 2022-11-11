0f0d3612945bb7d85ee86804b7513bfcfdb9e333
Senator kay <senatorkay2@gmail.com>
2022-11-11 14:47:42 +0100

    added some responsive styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import css from "./Header.module.css"
 import Logo from "../../assets/logo.png"
 import { CgShoppingBag } from 'react-icons/cg'
+import { GoThreeBars } from 'react-icons/go'
 
 const Header = () => {
+
+    const [showMenu, setShowMenu] = useState(true)
+
+    const toggleMenu =()=>{
+        setShowMenu((show)=>!show)
+    }
   return (
     <div className={css.container}>
         <div className={css.logo}>
             <img src= {Logo} alt="" />
             <span>amazon</span>
-
         </div>
+
+          
+
         <div className={css.right}>
-            <div className={css.menu}>
-                <ul className={css.menu}>
+      
+           <div className={css.bars} onClick={toggleMenu}>
+            <GoThreeBars/>
+           </div>
+
+
+           
+                <ul className={css.menu}  style={{display:showMenu ? 'inherit' : 'none'}}>
                     <li>Collection</li>
                     <li>Brands</li>
                     <li>New</li>
@@ -21,7 +36,6 @@ const Header = () => {
                     <li>ENG</li>
                 </ul>
 
-            </div>
             <input type="text" className= {css.search} placeholder = "search" />
 
             <CgShoppingBag className= {css.cart}/>
